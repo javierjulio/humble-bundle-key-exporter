f17f953f500f8e4930127414b28c21a68bb88613
Javier Julio <jjfutbol@gmail.com>
2018-08-05 19:07:42 -0400

    Comment out all tests for now

--- test/specs/core.specs.js
+++ test/specs/core.specs.js
@@ -1,15 +1,15 @@
-describe('Core', function() {
-  it('HumbleBundleScraper exsists', () => expect(HumbleBundleScraper).is.not.undefined )
+// describe('Core', function() {
+//   // it('HumbleBundleScraper exsists', () => expect(HumbleBundleScraper).is.not.undefined )
 
-  // it('it can send friendly messages', () => {
-  //   var greeter = new Greeter()
-  //   expect(greeter.message).is.equal('hi there Dear Coder!')
-  //   // these white spaces will be trimmed
-  //   greeter.message = '   goodbye         '
-  //   expect(greeter.message).is.equal('goodbye Dear Coder!')
-  // })
+//   // it('it can send friendly messages', () => {
+//   //   var greeter = new Greeter()
+//   //   expect(greeter.message).is.equal('hi there Dear Coder!')
+//   //   // these white spaces will be trimmed
+//   //   greeter.message = '   goodbye         '
+//   //   expect(greeter.message).is.equal('goodbye Dear Coder!')
+//   // })
 
-  // it('retrieves absolute url', () => {
-  //   expect(Greeter.absoluteUrl('/test')).is.equal('/test')
-  // })
-})
+//   // it('retrieves absolute url', () => {
+//   //   expect(Greeter.absoluteUrl('/test')).is.equal('/test')
+//   // })
+// })
