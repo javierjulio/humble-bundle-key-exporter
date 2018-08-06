12d9882052c77825d9bd6f5c350ff0977795e9ba
Javier Julio <jjfutbol@gmail.com>
2018-08-05 20:35:12 -0400

    Apply single test case to verify test runner works

--- test/specs/core.specs.js
+++ test/specs/core.specs.js
@@ -1,15 +1,16 @@
-// describe('Core', function() {
-//   // it('HumbleBundleScraper exsists', () => expect(HumbleBundleScraper).is.not.undefined )
+describe('Core', function() {
+  it('adds', () => expect(1 + 1).is.equal(2) )
+  // it('HumbleBundleScraper exsists', () => expect(HumbleBundleScraper).is.not.undefined )
 
-//   // it('it can send friendly messages', () => {
-//   //   var greeter = new Greeter()
-//   //   expect(greeter.message).is.equal('hi there Dear Coder!')
-//   //   // these white spaces will be trimmed
-//   //   greeter.message = '   goodbye         '
-//   //   expect(greeter.message).is.equal('goodbye Dear Coder!')
-//   // })
+  // it('it can send friendly messages', () => {
+  //   var greeter = new Greeter()
+  //   expect(greeter.message).is.equal('hi there Dear Coder!')
+  //   // these white spaces will be trimmed
+  //   greeter.message = '   goodbye         '
+  //   expect(greeter.message).is.equal('goodbye Dear Coder!')
+  // })
 
-//   // it('retrieves absolute url', () => {
-//   //   expect(Greeter.absoluteUrl('/test')).is.equal('/test')
-//   // })
-// })
+  // it('retrieves absolute url', () => {
+  //   expect(Greeter.absoluteUrl('/test')).is.equal('/test')
+  // })
+})
