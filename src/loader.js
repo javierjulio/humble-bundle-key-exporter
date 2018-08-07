de40825affb002b2a95931e9fbe0d1777d367b2f
Javier Julio <jjfutbol@gmail.com>
2018-08-07 16:22:08 -0400

    Update loader log messages

--- src/loader.js
+++ src/loader.js
@@ -4,15 +4,16 @@ if (window.HumbleBundleKeyScraper === undefined) {
   const url = "//rawgit.com/javierjulio/humble-bundle-key-scraper/master/dist/humble-bundle-key-scraper.js"
 
   const script = document.createElement("script")
-  script.id = "__humbleBundleScraper"
+  script.id = "__humbleBundleKeyScraper"
   script.src = url + "?c=" + Math.floor((Math.random() * 10000) + 1)
   script.onload = function(){
-    console.log("HumbleBundleKeyScraper already loaded.", "Scraping...")
+    console.log("HumbleBundleKeyScraper loaded.")
+    console.log("Scraping...")
     HumbleBundleKeyScraper.run()
   }
 
   document.head.appendChild(script)
 } else {
-  console.log("HumbleBundleKeyScraper already loaded.", "Scraping...")
+  console.log("Scraping again...")
   HumbleBundleKeyScraper.run()
 }
