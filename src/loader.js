b3d2694a4ac773c9c2b33ede0f37640f3208e7ac
Javier Julio <jjfutbol@gmail.com>
2018-08-07 15:59:04 -0400

    Only load script if undefined, otherwise run it

--- src/loader.js
+++ src/loader.js
@@ -1,15 +1,18 @@
-console.log("Loading HumbleBundleKeyScraper...")
+if (window.HumbleBundleKeyScraper === undefined) {
+  console.log("Loading HumbleBundleKeyScraper...")
 
-// TODO: wrap in conditional, only run if HumbleBundleKeyScraper is undefined
+  const url = "//rawgit.com/javierjulio/humble-bundle-key-scraper/master/dist/humble-bundle-key-scraper.js"
 
-const url = "//rawgit.com/javierjulio/humble-bundle-scraper/master/dist/humble-bundle-key-scraper.js"
+  const script = document.createElement("script")
+  script.id = "__humbleBundleScraper"
+  script.src = url + "?c=" + Math.floor((Math.random() * 10000) + 1)
+  script.onload = function(){
+    console.log("HumbleBundleKeyScraper already loaded.", "Scraping...")
+    HumbleBundleKeyScraper.run()
+  }
 
-const script = document.createElement("script")
-script.id = "__humbleBundleScraper"
-script.src = url + "?c=" + Math.floor((Math.random() * 10000) + 1)
-script.onload = function(){
-  console.log("HumbleBundleKeyScraper loaded.", "Scraping...")
+  document.head.appendChild(script)
+} else {
+  console.log("HumbleBundleKeyScraper already loaded.", "Scraping...")
   HumbleBundleKeyScraper.run()
 }
-
-document.head.appendChild(script)
