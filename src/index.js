86a29e58b96fa006c33fc94e4408a9da50e18c89
Javier Julio <jjfutbol@gmail.com>
2022-12-14 18:43:37 -0500

    Inline helper methods

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
-import { delayOf, downloadFile } from './helpers/helpers.js'
+/**
+ * Returns a new Promise that is resolved by a timer for the given milliseconds.
+ * @param  { Number } ms - milliseconds
+ * @returns { Promise } - a promise that is resolved by a timer
+ */
+export function delayOf(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+/**
+ * Given the blob data and file name, the file is downloaded in the browser.
+ * @param  { String } data - the blob data
+ * @param  { String } name - the file name
+ */
+export function downloadFile(data, name) {
+  const a = document.createElement('a')
+  a.href = URL.createObjectURL(new Blob([data]))
+  a.download = name
+  a.click()
+  setTimeout(() => URL.revokeObjectURL(a.href), 500)
+}
 
 const hideRedeemed = async () => {
   const checkbox = document.getElementById('hide-redeemed')
